9956ffb2c74051381ac0f27e2601d0af25aa19b2
Hendy Irawan <hendy@soluvas.com>
2011-06-16 17:41:58 +0700

    initial import

---
+++ src/gogosimple1/Activator.java
@@ -0,0 +1,37 @@
+package gogosimple1;
+
+import java.util.logging.Logger;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+	Logger logger = Logger.getLogger(Activator.class.getName());
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		System.out.println("Yuhu");
+		logger.info("Hello from gogosimple1");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		System.out.println("Bye");
+		logger.info("Bye from gogosimple1");
+		Activator.context = null;
+	}
+
+}
---
+++ src/gogosimple1/SimpleCommand.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package gogosimple1;
+
+/**
+ * @author ceefour
+ *
+ */
+public class SimpleCommand {
+
+	public void walk() {
+		System.out.println("walking");
+	}
+	
+	public void talk() {
+		System.out.println("talking");
+	}
+}
